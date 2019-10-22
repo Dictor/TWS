8c2f391002685a35fdc6f5f164d7ee83d3ec2f98
김정현 <kimdictor@gmail.com>
2019-10-22 17:31:23 +0900

    태스크, 구조체, 열거형 수정
    * 태스크를 목적에 맞게 2개로 정리
    * 필요한 전역 변수 정의
    * 필요한 구조체, 열거형 정의

--- TWS.ino
+++ TWS.ino
@@ -2,23 +2,42 @@
 #include "pin_config.h"
 
 /* Task declaration */
-void tskReadAnalog(void *pvParameters);
-void tskReadDHT(void *pvParameters);
-void tskSendData(void *pvParameters);
-void tskCalculateGlare(void *pvParameters);
+void tskSendStaticSensor(void *pvParameters);
+void tskMonitorDynamicSensor(void *pvParameters);
 
 /* Value struct declaration */
-typedef struct {
+typedef struct _CdsValue {
+  long time;
   int vertical;
   int horizon1;
   int horizon2;
   int horizon3;
 } CdsValue;
 
-typedef struct {
+typedef struct _StaticSensorValue {
   long time;
-  int value;
-} SensorValue;
+  int water;
+  int sound;
+  int temp;
+  int humi;
+} StaticSensorValue;
+
+enum DynamicSensorKind {
+  KIND_GLARE,
+  KIND_ANTENNA
+};
+
+typedef struct _DynamicSensorEvent {
+  DynamicSensorKind kind;
+  char data;
+} DynamicSensorEvent;
+
+/*Global variable declaration*/
+QueueHandle_t CdsDerivative; //Cds 변화율 적재 큐
+CdsValue LastCdsValue; //가장 최근 Cds 값
+byte LastGlareCount; //가장 최근 주기 섬광 감시 횟수
+QueueHandle_t DynamicSensorEvents; //동적 센서 이벤트 적재 큐 
+StaticSensorValue LastStaticSensorValue; //가장 최근 정적 센서 값
 
 void setup() {
 
