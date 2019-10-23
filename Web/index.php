d4f675d70d891d55701d1babe367efae0fe83194
김정현 <kimdictor@gmail.com>
2019-10-23 15:10:54 +0900

    로그 페이지 구현

--- Web/index.php
+++ Web/index.php
@@ -1,3 +1,7 @@
+<?php
+    namespace Dictor\OSAM2019;
+    require_once('lib/db.php');
+?>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -15,20 +19,49 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head> 	
     <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">TWS</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">대시보드 <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">로그</a>
-            </li>
-        </div>
+        <script>
+            <?php 
+                if (Model::UseDB() != 0) {
+                    echo 'alert("DB ERROR!");';
+                    exit();
+                }
+                echo 'var jsLog = '.json_encode(Model::GetLogs(-1)).";\n";
+                echo '            var jsEvt = '.json_encode(Model::GetEvents(-1)).";\n";
+                echo '            var jsSen = '.json_encode(Model::GetSensorData(-1)).";\n";
+            ?>
+        </script>   
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">TWS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">대시보드 <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">로그</a>
+                </li>
+            </div>
         </nav>
+        <div id="page-dashboard"></div>
+        <div id="page-log">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">시간</th>
+                        <th scope="col">내용</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        foreach(Model::GetLogs(-1) as $nowlog) {
+                            echo '<tr><th scope="row">'.$nowlog['time'].'</th><td>'.$nowlog['data'].'</td></tr>';
+                        }
+                    ?>
+                </tbody>
+            </table>    
+        </div>
     </body>
 </html>
