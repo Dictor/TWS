63b611ba86b067601d7d41dbe5c83fee49123002
김정현 <kimdictor@gmail.com>
2019-10-23 22:07:11 +0900

    오타 수정 ($ 누락)

--- Web/lib/db.php
+++ Web/lib/db.php
@@ -101,7 +101,7 @@
 				case 'LOG': DB::Query("INSERT INTO log(time, data) VALUES (:itime, :idata)", array('itime' => $date, 'idata' => $data[0])); break;
 				case 'SENSOR': DB::Query("INSERT INTO sensor(time, temp, humi, water, sound) VALUES (:itime, :itemp, :ihumi, :iwater, :isound)", 
 											array('itime' => $date, 'itemp' => (int)$data[0], 'ihumi' => (int)$data[1], 'iwater' => (int)$data[2], 'isound' => (int)$data[3])); break;
-				case 'EVENT': DB::Query("INSERT INTO event(time, kind, data) VALUES (:itime, :ikind, :idata)", array('itime' => $date, 'ikind' => $data[0], 'idata' => (int)data[1])); break;
+				case 'EVENT': DB::Query("INSERT INTO event(time, kind, data) VALUES (:itime, :ikind, :idata)", array('itime' => $date, 'ikind' => $data[0], 'idata' => (int)$data[1])); break;
 				default:
 			}
 		}
